bd1fbea2352274ca4c1ac8c49925d4b11a0796d2
Ruiwen Zhao <riven012303@gmail.com>
2020-05-11 08:00:16 +0800

    add Densenet121 model

---
+++ model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+class DenseNet121(nn.Module):
+
+    def __init__(self):
+	
+        super(DenseNet121, self).__init__()
+		
+        self.densenet121 = torchvision.models.densenet121(pretrained=False)
+
+        kernelCount = self.densenet121.classifier.in_features
+		
+        self.densenet121.classifier = nn.Sequential(nn.Linear(kernelCount, 1), nn.Sigmoid())
+
+    def forward(self, x):
+        x = self.densenet121(x)
+        return x
+
+class Vgg(nn.Module):
