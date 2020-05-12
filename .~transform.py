7e36550d507e5a06b007fbd422f2815aaa4f541d
Ruiwen Zhao <riven0123032gmail.com>
2020-05-12 02:57:55 +0000

    cleanup

--- .~transform.py
+++ .~transform.py
@@ -1,5 +1,3 @@
-from torchvision.datasets import ImageFolder
-from torchvision import transforms
 import numpy as np
 from PIL import Image
 from random import random
@@ -107,29 +105,3 @@ class Smote(CustomTransforms):
                 new += random() * (nearest - img)
             new = Image.fromarray(np.uint8(new))
             return [new, 0]
-
-
-class CustomFolder(ImageFolder):
-    
-    def __getitem__(self, index):
-        path, target = self.samples[index]
-        sample = self.loader(path)
-        if self.transform is not None:
-            sample = self.transform((sample, target))
-
-        return sample
-    
-
-class CustomCompose(transforms.Compose):
-    
-    def __call__(self, sample):
-        img = sample[0]
-        label = sample[1]
-        for t in self.transforms:
-            if isinstance(t, CustomTransforms):
-                img, label = t((img, label))
-            else:
-                img = t(img)
-        return img, label
-        
-
